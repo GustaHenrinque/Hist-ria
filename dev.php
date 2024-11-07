e7cc09a359ddd5e91fc505499d30f9abc800a144
rianeduzz <rianedupz@gmail.com>
2024-11-07 09:21:32 -0300

    atualizeiii

--- dev.php
+++ dev.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Equipe de Desenvolvedores</title>
-    <link rel="stylesheet" href="css.css">
+    <link rel="stylesheet" href="dev.css">
     <script src="devs.js" defer></script>
 </head>
 <body>
